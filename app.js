64e0d1774085d0c4b44ea45f05546d5e66689cd6
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2025-07-24 16:59:45 +0300

    commented client.on(ready...

--- app.js
+++ app.js
@@ -613,37 +613,37 @@ async function sendWhatsAppSummary(summary) {
   }
 }
 
-client.on("ready", async () => {
-  console.log("✅ WhatsApp client is ready.");
+// client.on("ready", async () => {
+//   console.log("✅ WhatsApp client is ready.");
 
-  const testNumber = process.env.WHATSAPP_TARGET;
-  if (!testNumber) return;
+//   const testNumber = process.env.WHATSAPP_TARGET;
+//   if (!testNumber) return;
 
-  const formattedNumber = `${testNumber}@c.us`;
+//   const formattedNumber = `${testNumber}@c.us`;
 
-  try {
-    await delay(3000);
-    console.log("👉 Trimit mesaj către:", formattedNumber);
+//   try {
+//     await delay(3000);
+//     console.log("👉 Trimit mesaj către:", formattedNumber);
 
-    await client.sendMessage(formattedNumber, "Test mesaj scurt cu delay 🚀");
+//     await client.sendMessage(formattedNumber, "Test mesaj scurt cu delay 🚀");
 
-    const summary = await run(true);
-    await sendWhatsAppSummary(summary);
-    console.log("📤 Rezumat trimis pe WhatsApp!");
+//     const summary = await run(true);
+//     await sendWhatsAppSummary(summary);
+//     console.log("📤 Rezumat trimis pe WhatsApp!");
 
-    // 🔚 Așteaptă 60 de secunde și apoi închide aplicația complet
-    setTimeout(() => {
-      console.log("⏹️ Închidere aplicație după trimitere...");
-      process.exit(0);
-    }, 2000);
-  } catch (err) {
-    console.error(
-      "❌ Eroare la trimiterea mesajului pe WhatsApp:",
-      err.message
-    );
-    process.exit(1); // oprește cu cod de eroare dacă e cazul
-  }
-});
+//     // 🔚 Așteaptă 60 de secunde și apoi închide aplicația complet
+//     setTimeout(() => {
+//       console.log("⏹️ Închidere aplicație după trimitere...");
+//       process.exit(0);
+//     }, 60000);
+//   } catch (err) {
+//     console.error(
+//       "❌ Eroare la trimiterea mesajului pe WhatsApp:",
+//       err.message
+//     );
+//     process.exit(1); // oprește cu cod de eroare dacă e cazul
+//   }
+// });
 
 client.on("message", (msg) => {
   console.log("📩 Mesaj primit:", msg.body);
